a38e48699dbd91a9dc376ce0f83cab85b71c831d
richard <richardjgowers@gmail.com>
2022-12-03 17:35:01 +0000

    is this breaking the docs build?

--- cinnabar/__init__.py
+++ cinnabar/__init__.py
@@ -11,16 +11,3 @@ versions = get_versions()
 __version__ = versions["version"]
 __git_revision__ = versions["full-revisionid"]
 del get_versions, versions
-
-from . import stats
-from . import wrangle
-from .wrangle import (
-    RelativeResult,
-    ExperimentalResult,
-    FEMap, read_csv,
-)
-from . import plotlying
-from . import plotting
-
-# the cli entrypoint
-from . import arsenic
