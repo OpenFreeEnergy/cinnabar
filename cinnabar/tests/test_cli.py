95debae59ab29d3b0acb95a062d6d6321a1968be
richard gowers <richardjgowers@gmail.com>
2023-03-06 19:33:18 +0000

    add cli smoke tests for each plot function

--- cinnabar/tests/test_cli.py
+++ cinnabar/tests/test_cli.py
@@ -4,7 +4,14 @@ import subprocess
 
 def test_cli_smoke():
     # check that running the help command doesn't explode
-    p = subprocess.call(['cinnabar', '-h'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.run(['cinnabar', '-h'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    assert p == 0
+    assert p.returncode == 0, p.stderr
 
+
+@pytest.mark.parametrize('mode', ['all', 'ddg', 'dg', 'all ddg', 'network'])
+def test_cli_plot(mode, example_csv):
+    p = subprocess.run(['cinnabar', example_csv, '--plot', mode],
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    assert p.returncode == 0, p.stderr
