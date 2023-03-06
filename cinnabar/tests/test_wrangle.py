2e4e710654ee7d3ea38319f4f66d7200c484aefe
richard gowers <richardjgowers@gmail.com>
2023-03-06 10:56:53 +0000

    more tests for FEMap

--- cinnabar/tests/test_wrangle.py
+++ cinnabar/tests/test_wrangle.py
@@ -1,6 +1,7 @@
 from importlib import resources
 import pytest
 
+from openff.units import unit
 from cinnabar import wrangle
 
 
@@ -18,3 +19,20 @@ def test_read_csv(example_csv):
 
     assert 'Calculated' in data
     assert len(data['Calculated']) == 58
+
+
+def test_femap():
+    m = wrangle.FEMap()
+
+    m1 = wrangle.RelativeMeasurement(labelA='ligA', labelB='ligB', DDG=1.1 * unit.kilojoule_per_mole,
+                                     uncertainty=0.1 * unit.kilojoule_per_mole, computational=True)
+    m2 = wrangle.AbsoluteMeasurement(label='ligA', DG=10.0 * unit.kilojoule_per_mole,
+                                     uncertainty=0.2 * unit.kilojoule_per_mole, computational=False)
+    m3 = wrangle.AbsoluteMeasurement(label='ligB', DG=11.0 * unit.kilojoule_per_mole,
+                                     uncertainty=0.3 * unit.kilojoule_per_mole, computational=False)
+
+    m.add_measurement(m1)
+    m.add_measurement(m2)
+    m.add_measurement(m3)
+
+    assert m.n_ligands == 2
