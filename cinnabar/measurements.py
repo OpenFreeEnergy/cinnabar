7c3e47bf2df496daf02d8d185645dfbf9e9b7245
richard gowers <richardjgowers@gmail.com>
2023-03-07 13:53:41 +0000

    default unit set to kcal

--- cinnabar/measurements.py
+++ cinnabar/measurements.py
@@ -6,15 +6,15 @@ class RelativeMeasurement(DefaultModel):
     """The free energy difference of moving from A to B"""
     labelA: str
     labelB: str
-    DDG: FloatQuantity['kilojoule_per_mole']
-    uncertainty: FloatQuantity['kilojoule_per_mole']
+    DDG: FloatQuantity['kilocalorie_per_mole']
+    uncertainty: FloatQuantity['kilocalorie_per_mole']
     computational: bool
     source: str = ""
 
 
 class AbsoluteMeasurement(DefaultModel):
     label: str
-    DG: FloatQuantity['kilojoule_per_mole']
-    uncertainty: FloatQuantity['kilojoule_per_mole']
+    DG: FloatQuantity['kilocalorie_per_mole']
+    uncertainty: FloatQuantity['kilocalorie_per_mole']
     computational: bool
     source: str = ""
