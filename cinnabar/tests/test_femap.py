44e6657b3edd302f95fd9590988ff3635e8da21d
richard gowers <richardjgowers@gmail.com>
2023-03-07 09:58:15 +0000

    use global example_csv fixture

--- cinnabar/tests/test_femap.py
+++ cinnabar/tests/test_femap.py
@@ -7,12 +7,6 @@ import cinnabar
 from cinnabar import femap
 
 
-@pytest.fixture
-def example_csv():
-    with resources.path('cinnabar.data', 'example.csv') as f:
-        yield f
-
-
 def test_read_csv(example_csv):
     data = femap.read_csv(example_csv)
 
