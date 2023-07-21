14280f29f1d1cc9c43fc3d991139b4e84869834b
hannahbaumann <hbaumann@uci.edu>
2023-07-21 14:42:20 +0200

    Add test for from_experiment function

--- cinnabar/tests/test_measurements.py
+++ cinnabar/tests/test_measurements.py
@@ -1,4 +1,6 @@
 import cinnabar
+import pytest
+from openff.units import unit
 
 
 def test_ground():
@@ -10,3 +12,14 @@ def test_ground():
     assert g1 == g2
     assert g1 != g3
     assert g3 == g4
+
+def test_Ki_to_DG():
+    Ki = 100 * unit.nanomolar
+    uncertainty = 10 * unit.nanomolar
+    dG = -9.55 * unit.kilocalorie_per_mole
+    dG_uncertainty = 0.059 * unit.kilocalorie_per_mole
+    label = 'lig'
+    Ki_to_DG = cinnabar.Measurement.from_experiment(Ki, label, uncertainty)
+
+    assert pytest.approx(dG, 0.001) == Ki_to_DG.DG
+    assert pytest.approx(dG_uncertainty, 0.01) == Ki_to_DG.uncertainty
