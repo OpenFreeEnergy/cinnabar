cfbc7c357b4067c905d0f3bdd84d428d631052b4
Josh Mitchell <yoshanuikabundi@gmail.com>
2023-09-28 13:40:38 +1000

    Switch to OFE theme

--- docs/conf.py
+++ docs/conf.py
@@ -95,8 +95,11 @@ html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#
-# html_theme_options = {}
+
+html_theme = "ofe_sphinx_theme"
+html_theme_options = {
+    "accent_color": "FeelingSpicy",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
