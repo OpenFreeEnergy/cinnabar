b57cf4c11ffb724b117918eb164536f4f5f322cd
richard gowers <richardjgowers@gmail.com>
2023-12-11 15:46:34 +0000

    adds base estimator class for FEMap

--- cinnabar/estimators/base.py
+++ cinnabar/estimators/base.py
@@ -1,2 +1,10 @@
-class BaseEstimator:
-    pass
+import abc
+
+from .. import FEMap
+
+
+class BaseEstimator(abc.ABC):
+    """base class for implementing custom estimators"""
+    @abc.abstractmethod
+    def estimate(self, prior: FEMap) -> FEMap:
+        pass
