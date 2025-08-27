8a470ff2b9513c3e08dd22b86d974925b5884741
Josh Horton <Josh.Horton@newcastle.ac.uk>
2025-08-27 14:35:36 +0100

    add due credit for mle method

---
+++ cinnabar/tests/test_due.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+from cinnabar._due import due
+
+
+pytest.importorskip("duecredit")
+
+def test_duecredit_mle():
+    """Make sure duecredit is captured when the stats module is used"""
+    mle_key = ("cinnabar.stats.mle", "10.1021/acs.jcim.9b00528")
+    assert due.citations[mle_key].cites_module
