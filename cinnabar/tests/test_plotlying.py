994aaf221e37f514ebfaf82a6ceb256402f99be0
Josh Horton <Josh.Horton@newcastle.ac.uk>
2026-01-14 12:23:20 +0000

    as plotly tests

---
+++ cinnabar/tests/test_plotlying.py
@@ -0,0 +1,51 @@
+from cinnabar import plotting
+from cinnabar import plotlying
+import pytest
+import plotly.graph_objects as go
+
+def test_plot_ddgs_plotly(tmp_path, fe_map):
+    output_file = tmp_path / "ddg_plot.html"
+    _ = plotting.plot_DDGs(fe_map.to_legacy_graph(), filename=output_file.as_posix(), plotly=True)
+    assert output_file.exists()
+
+
+def test_plot_dgs_plotly(tmp_path, fe_map):
+    output_file = tmp_path / "dg_plot.html"
+    _ = plotting.plot_DGs(fe_map.to_legacy_graph(), filename=output_file.as_posix(), plotly=True)
+    assert output_file.exists()
+
+
+def test_plot_all_ddgs_plotly(tmp_path, fe_map):
+    output_file = tmp_path / "all_ddg_plot.html"
+    _ = plotting.plot_all_DDGs(fe_map.to_legacy_graph(), filename=output_file.as_posix(), plotly=True)
+    assert output_file.exists()
+
+
+def test_master_plot_bad_statistic_type(example_data):
+    """Test that bad statistic in master plot raises an error."""
+
+    x_data, y_data, xerr, yerr = example_data
+    with pytest.raises(ValueError, match="Unknown statistic type bad_stat"):
+        _ = plotlying._master_plot(
+            x_data,
+            y_data,
+            statistic_type="bad_stat",
+        )
+
+def test_master_plot_show(example_data, monkeypatch):
+    """Test that master plot shows when filename is None."""
+
+    x_data, y_data, xerr, yerr = example_data
+    called = {}
+
+    def mock_show(self):
+        called["show"] = True
+
+    monkeypatch.setattr(go.Figure, "show", mock_show)
+
+    _ = plotlying._master_plot(
+        x_data,
+        y_data,
+        filename=None,
+    )
+    assert "show" in called
