ecbb33ced7e53c29ad988c622ebfedc8040c2c58
Josh Horton <joshua.horton@openforcefield.org>
2026-05-26 12:06:35 +0100

    Update cinnabar/femap.py
    Co-authored-by: Irfan Alibay <IAlibay@users.noreply.github.com>

--- cinnabar/femap.py
+++ cinnabar/femap.py
@@ -29,7 +29,7 @@ if TYPE_CHECKING:
 _kcalpm = unit.kilocalorie_per_mole
 
 
-def read_csv(filepath: pathlib.Path, units: Quantity | None = None) -> dict:
+def read_csv(filepath: pathlib.Path, units: Quantity | None = None) -> dict[str, dict[Hashable, Measurement] | list[[Measurement]]
     """Read a legacy format csv file
 
     Parameters
