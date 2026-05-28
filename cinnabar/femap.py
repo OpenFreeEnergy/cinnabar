9ff8318862a895cf6a65962a92c72142db3f4046
hannahbaumann <hbaumann@uci.edu>
2026-05-28 14:56:16 +0200

    Add helper function

--- cinnabar/femap.py
+++ cinnabar/femap.py
@@ -867,6 +867,11 @@ class FEMap:
 
         return g
 
+    @staticmethod
+    def _canonical_edge(edge: tuple[str, str]) -> tuple[str, str]:
+        a, b = sorted(edge)
+        return a, b
+
     def draw_graph(
         self,
         title: str = "",
@@ -891,13 +896,13 @@ class FEMap:
         if highlight_edges:
             for color, edges in highlight_edges.items():
                 for edge in edges:
-                    edge_to_color[sorted(edge)] = color
+                    edge_to_color[self._canonical_edge(edge)] = color
 
         fig, ax = plt.subplots(figsize=(10, 10))
         graph = self.to_legacy_graph()
         labels = {n: n for n in graph.nodes}
 
-        graph_edges = [sorted(edge) for edge in graph.edges()]
+        graph_edges = [self._canonical_edge(edge) for edge in graph.edges()]
         edge_colors = [edge_to_color.get(edge, "grey") for edge in graph_edges]
         edge_widths = [2.5 if edge in edge_to_color else 1.0 for edge in graph_edges]
 
